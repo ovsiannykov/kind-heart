d3f8b7a8052f9caef5475d372ac4753f293a51c4
Mike <mykhailo.ovsiannykov@isquad.dev>
2022-03-28 22:56:39 +0300

    Add Hero Section

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -2,8 +2,26 @@ import React from "react";
 
 import "./index.css";
 
+const heroImage = require("../../assets/img/hero-image.png");
+
 const Hero = () => {
-  return <div class="container">hello</div>;
+  return (
+    <div class="container">
+      <div className="hero">
+        <div className="title-box">
+          <h1 className="title">Допомагати – це круто</h1>
+          <div className="sub-title">
+            <p>Благодійний фонд когось там</p>
+          </div>
+          <button className="hero-button btn">Детальніше</button>
+          {/* <a className="hero-button">Детальніше</a> */}
+        </div>
+        <div className="image-box">
+          <img className="image" src={heroImage} alt="hero" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
---
+++ src/constants/colors.js
@@ -0,0 +1,1 @@
+export const primary = "#b39ddb";
