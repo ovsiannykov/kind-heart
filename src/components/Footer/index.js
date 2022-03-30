c40fa7a9f58d0a7502d2f5d025d620ed00f911d5
Mike <mykhailo.ovsiannykov@isquad.dev>
2022-03-30 15:39:51 +0300

    add fonts

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,13 +2,18 @@ import React from "react";
 
 import "./index.css";
 
+const footerClick = (e) => {
+  e.preventDefault();
+  window.open("https://t.me/mikie_mac");
+};
+
 const Footer = () => {
   return (
     <>
       <footer>
         <span>
           Created by{" "}
-          <a href="https://t.me/mikie_mac" target="_blank">
+          <a href="https://t.me/mikie_mac" onClick={footerClick}>
             ovsiannykov
           </a>
         </span>
